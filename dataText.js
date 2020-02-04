463a755eac1a59342b59e4a01516b702252e709c
Suchron Rizal <suchronrizal@gmail.com>
2020-02-04 16:42:45 +0700

    first commit

---
+++ dataText.js
@@ -0,0 +1,59 @@
+//stars
+const textStars = [
+  { id: "1", dataText: "Pelayanan Sangat Buruk" },
+  { id: "2", dataText: "Pelayanan Buruk" },
+  { id: "3", dataText: "Pelayanan Cukup Cepat" },
+  { id: "4", dataText: "Pelayanan Bagus" },
+  { id: "5", dataText: "Pelayanan Sangat Bagus" }
+];
+//items optional
+const itemsOptional = [
+  {
+    id: "1",
+    arrName: [
+      "Mobil Sangat Jorok",
+      "Sangat Tidak Nyaman",
+      "Sangat Bau",
+      "Sopir Sangat Tidak Ramah",
+      "Sopir Sangat Tidak Profesional",
+      "Navigasi Sangat Buruk"
+    ]
+  },
+  {
+    id: "2",
+    arrName: [
+      "Mobil Jorok",
+      "Tidak Nyaman",
+      "Mobil Bau",
+      "Sopir Tidak Ramah",
+      "Pengemudi Tidak Profesional",
+      "Navigasi Buruk"
+    ]
+  },
+  {
+    id: "3",
+    arrName: [
+      "Cukup Bersih",
+      "Cukup Nyaman",
+      "Cukup Wangi",
+      "Cukup Ramah",
+      "Cukup Aman Mengemudi",
+      "Cukup Menguasai Navigasi"
+    ]
+  },
+  {
+    id: "4",
+    arrName: ["Bersih", "Nyaman", "Wangi", "Sopir Ramah", "Sopir Profesional", "Baik Navigasi"]
+  },
+  {
+    id: "5",
+    arrName: [
+      "Sangat Bersih",
+      "Sangat Nyaman",
+      "Sangat Wangi",
+      "Driver Sangat Ramah",
+      "Driver Sangat Profesional",
+      "Expert Navigasi"
+    ]
+  }
+];
